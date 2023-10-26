131ef524fd56f0029d91d57850be31f0ae270e88
saaketht <saakethtallam@gmail.com>
2023-10-26 16:37:43 -0400

    log

--- server/index.js
+++ server/index.js
@@ -69,6 +69,7 @@ if (process.env.NODE_ENV === 'production')
   // Set static folder
   app.use(express.static('client/build'));
   // Serve index.html file if it exists on the server (production)
+  console.log("Serving index.html file from: " + path.resolve(__dirname, 'client', 'build', 'index.html'));
   app.get('*', (req, res) =>
   {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
